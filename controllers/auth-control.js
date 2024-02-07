7f69946eda27047b9f1144e6f102ddb4368d435c
rehan749 <mahtab91250@gmail.com>
2024-02-07 15:06:21 +0530

    usermodel

--- controllers/auth-control.js
+++ controllers/auth-control.js
@@ -1,3 +1,6 @@
+const User = require('../model/userModel');
+const bcrypt = require('bcryptjs');
+
 const home = async (req, res) => {
     try {
         res.status(200).send("welcome to home page");
@@ -5,15 +8,24 @@ const home = async (req, res) => {
         console.log(error)
     }
 }
+
 const register = async (req, res) => {
     try {
-        console.log(req.body)
-        // res.status(200).send("welcome to register page");
-        res.status(200).json({ message: req.body });
+        console.log(req.body);
+        const { name, email, number, password } = req.body;
+        const userExist = await User.findOne({ email: email });
+        if (userExist) {
+            return res.status(400).json({ error: "Email already exists" });
+        }
+        //  Hashing the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const userCreated = await User.create({ name, email, number, password: hashedPassword });
+        res.status(200).json({ success: true, data: userCreated });
     } catch (error) {
-        req.status(500).json("interval server error");
-        console.log(error);
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
     }
 }
+
 
 module.exports = { home, register };
